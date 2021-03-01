d0282aa591db2f6647006cbcf18be7b7cd42c17a
Lucas Armanelli <luc.armanelli@gmail.com>
2021-03-01 15:32:11 -0300

    changes for heroku round 4

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,19 @@
 const { Pool } = require('pg')
 
-module.exports = new Pool ({
+let dbConfig = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     database: process.env.DB_NAME
-})
+};
+
+if (process.env.NODE_ENV !== 'development') {
+    dbConfig.ssl = {
+        rejectUnauthorized: false
+    }
+}
+
+console.log(process.env.NODE_ENV)
+
+module.exports = new Pool(dbConfig);
